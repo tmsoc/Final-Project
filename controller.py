d1a30d71216478513ccb568bfffe1ae6fa4665b4
Tony Samaniego <tms.ocmp@gmail.com>
2020-05-01 16:07:45 -0700

    cleaned up some functions

--- controller.py
+++ controller.py
@@ -313,15 +313,7 @@ class Controller:
 
     def user_view_more_info_press(self):
         """
-        This method creates a list that has all information of one restaurant
-        that user clicks on in the listbox, then opens a new window through
-        user_view_restaurant_info_window() in view class, passes this list into 
-        the method
         
-        rest_info_list = list()
-        ....
-        self.view.clear_frame()
-        self.view.user_view_restaurant_info_window(rest_info_list)
         """
         list_box = self.view.view3_list_box
         selected_rest = self._get_list_box_selection(list_box)
@@ -494,45 +486,6 @@ class Controller:
         elif import_file.is_file():
             self.display_error_message("Invalid file type. Must be a .pdf")
 
-    def validate_owner_login(self, username: str, password: str) -> bool:
-
-        login_info = self.model.owner_select_by_name(username)
-
-        if login_info is None:
-            return False
-
-        if login_info["password"] == password:
-            return True
-        else:
-            return False
-
-    def validate_admin_login(
-        self, entry_user_name: str, entry_password: str
-    ) -> bool:
-        username = entry_user_name.get()
-        password = entry_password.get()
-        login_info = self.model.admin_select(username)
-
-        if login_info is None:
-            return False
-
-        if login_info["password"] == password:
-            return True
-        else:
-            return False
-
-    def validate_user_login(self, username: str, password: str) -> bool:
-
-        login_info = self.model.user_select_by_name(username)
-
-        if login_info is None:
-            return False
-
-        if login_info["password"] == password:
-            return True
-        else:
-            return False
-
     def display_owner_id(self) -> list:
 
         restaurant_list = self.model.restaurants_select_all()
@@ -561,14 +514,3 @@ class Controller:
             restaurant_info_list.append(restaurant_info_str)
 
         return restaurant_info_list
-
-    # def menu_info(self) -> list:
-
-    #     menu_list = self.model.menus_select_all()
-    #     menu_info_list = []
-
-    #     for menu in menu_list:
-    #         menu_info_str = restaurant["menu"] # This line gives an error
-    #         menu_info_list.append(menu_info_str)
-
-    #     return menu_info_list
